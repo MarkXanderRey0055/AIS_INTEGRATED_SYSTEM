f61d90a9b3d860c325c03b2221bad6db99322186
MarkXanderRey0055 <23-10642@g.batstate-u.edu.ph>
2026-04-14 18:46:06 +0800

    first commit

---
+++ models/UserModel.js
@@ -0,0 +1,87 @@
+import pool from '../models/db.js';
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const getUser = async (id) =>{
+    const userId = Number(id);
+    if (Number.isNaN(userId)){
+        throw new Error('Invalid id');
+    }
+    const [rows] = await pool.query('SELECT * FROM tbluser WHERE id = ?', [userId]);
+    return rows[0] ?? null;
+}
+export const createUser = async (userProfile, email, password) =>{
+    if(!email || email.trim() === ''){
+        throw new Error('Invalid email');
+    }
+
+    if(validator.isEmail(email)){
+        throw new Error('Invalid email format');
+    }
+
+    const [rows] = await pool.query(
+        "SELECT * FROM tbluser WHERE email = ?",
+        [email]
+    );
+
+    if(rows.length > 0){
+        const error = new Error(`The email ${email} is already in use.`);
+        error.statusCode = 400;
+        throw error;
+    }
+
+    if(!password || password === ''){
+        throw new Error('Invalid password');
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw new Error('Password too weak.');
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+
+
+
+    const newPassword = bcrypt.hashSync(password, salt);
+   
+    const response = await fetch(
+        `https://ais-simulated-legacy.onrender.com/api/students`, {
+            method: "POST",
+            headers: {
+                 'Content-Type': 'application/json'
+
+        }, 
+        body : JSON.stringify(userProfile)
+
+    });
+
+    const [result] = await pool.query(
+        "INSERT INTO tbluser(email, password) VALUES(?, ?)",
+        [email, newPassword]
+    );
+
+    return result.insertId;
+}
+
+export const login = async (email, password) =>{
+    if(!email || !password){
+        throw new Error('Email and Password is required');
+    }
+
+    const [user] = await pool.query("SELECT * FROM tbluser WHERE email = ?", [email]);
+    if(user.length === 0){
+        throw new Error(`An Account with email: ${email} does not exist.`);
+    }
+
+    if(!bcrypt.compareSync(password, user[0].password)){
+        throw new Error('Incorrect password');
+    }
+
+    //generate token
+    const token = jwt.sign({id: user[0].id}, process.env.SECRET,{expiresIn: '1d'});
+
+
+    return token;
+    // return user[0].id;
+}
