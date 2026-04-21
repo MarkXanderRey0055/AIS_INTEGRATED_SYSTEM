c53d43fefd0613eda5733452453fa8846e3f20b1
MarkXanderRey0055 <23-10642@g.batstate-u.edu.ph>
2026-04-21 19:03:21 +0800

    fixes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,42 +1,55 @@
 import * as UserModel from "../models/UserModel.js";
 
-export const register = async (req, res) =>{
-    const {
-        firstName,
-        lastName,
-        birthdate,
-        address,
-        course,
-        major,
-        studentStatus,
-        email,
-        password} = req.body;
+export const register = async (req, res) => {
+    const { 
+        email, 
+        password,
+        firstName, 
+        lastName, 
+        dob, 
+        course, 
+        major, 
+        address, 
+        status 
+    } = req.body; 
 
-    try{
-        const userProfile = {firstName, lastName, birthdate, address, course, major, studentStatus};
-        const user = await UserModel.createUser(userProfile, email, password);
-        res.status(201).json({success: true, message: user});
-    }catch(err){
+    try {
+        const userProfile = {
+            firstName, 
+            lastName, 
+            dob, 
+            course, 
+            major, 
+            address, 
+            status
+        };
+
+       
+        const userId = await UserModel.createUser(userProfile, email, password);
+        
+        res.status(201).json({
+            success: true, 
+            message: "User registered successfully", 
+            id: userId
+        });
+    } catch (err) {
         console.log(err);
-
-        res.status(400).json({success: false, message: err});
+        res.status(400).json({ success: false, message: err.message });
     }
 }
 
-export const login = async (req, res) =>{
-    const {email, password} = req.body;
+export const login = async (req, res) => {
+    const { email, password } = req.body; 
 
-    try{
+    try {
         const token = await UserModel.login(email, password);
         res.status(200).json({
-            success: true,
-            message: [
-                {result: "Login Successful"}
-            ],
-            token: token
+            success: true, 
+            message: [{ result: "Login successful" }], 
+            token: token 
         });
-    }catch(err){
+    } catch (err) {
         console.log(err);
-        res.status(400).json({success: false, message: err});
+        res.status(400).json({ success: false, message: err.message });
     }
 }
